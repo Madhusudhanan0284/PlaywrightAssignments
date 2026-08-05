212f61dbd75356e22dd4e84d9e6a11db9303a005
Madhusudhanan S. <53971504+Madhusudhanan0284@users.noreply.github.com>
2026-08-05 15:54:56 +0530

    Add files via upload
    Week1 Day3 Class Assignments

---
+++ Week1Day3/javascriptScoping.js
@@ -0,0 +1,9 @@
+const browserVersion = 'Chrome'
+function getBrowserVersion(){
+    if(browserVersion === 'Chrome'){
+        //var browserVersion = 'Safari'
+        let browserVersion = 'Safari'
+    }
+    console.log(browserVersion);
+}
+getBrowserVersion()
