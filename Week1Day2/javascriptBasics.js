137fcc6ddf90cfd6987a11690dc3b25ff425f042
Madhusudhanan S. <53971504+Madhusudhanan0284@users.noreply.github.com>
2026-08-05 15:54:22 +0530

    Add files via upload
    Week 1 Day 2 Class Assignments

---
+++ Week1Day2/javascriptBasics.js
@@ -0,0 +1,5 @@
+console.log("Welcome to Javascript learning");
+
+// Command to Execute the file : node filename.js
+//Shortcut to comment multiple line : Alt+Shift+a
+//Shortcut to comment single line : ctrl+/
---
+++ Week1Day2/javascriptDatatypes.js
@@ -0,0 +1,29 @@
+var firstName = 'Madhusudhanan'
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = 'Cognizant Technology Solutions'
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = '9548762354'
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
+
+
+
+
+
+
