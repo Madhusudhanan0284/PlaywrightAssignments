aff601a2180c0bc5d1f550e56e7a0d069da33baa
Madhusudhanan S. <53971504+Madhusudhanan0284@users.noreply.github.com>
2026-08-06 18:30:01 +0530

    Add files via upload
    Week1 Day 4 Homework Assignment

---
+++ Week1Day4/javascriptOddOrEven.js
@@ -0,0 +1,10 @@
+function isOddOrEven(number){
+        if(number%2 != 0){
+        console.log('Odd Number: ', number);   
+    }
+    else{
+        console.log('Even Number:', number);
+        
+    }
+}
+isOddOrEven(11)
